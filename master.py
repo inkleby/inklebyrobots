63008e5958cbdf8ded0178bb7a284f706cf694a6
Alex Parsons <alex@alexparsons.co.uk>
2015-09-26 08:08:13 +0100

    randomfoi added to master controller

--- master.py
+++ master.py
@@ -7,6 +7,7 @@ Created on 19 Sep 2015
 from classes import RobotMaster
 import oldillustration
 import badhousingideas
+import randomfoi
 import credentials
 r = RobotMaster()
 r.register_retweet(credentials.twitter_inklebyrobots)
