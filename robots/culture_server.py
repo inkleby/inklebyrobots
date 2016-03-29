b1ad56544ec60a275cb5c0deaaf08525c8f6361f
Alex Parsons <alex@alexparsons.co.uk>
2016-03-29 16:49:57 +0100

    added fix to allow robot to be run individually

--- robots/culture_server.py
+++ robots/culture_server.py
@@ -2,6 +2,9 @@
 Cultural Reuse Project - tweets the stored tweets off a schedule
 the culture_chef built earlier
 '''
+if __name__ == "_main__":
+    import sys
+    sys.path.append("..")
 
 import credentials
 from robot_core import Robot
